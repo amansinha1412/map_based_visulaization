b7190880f6fcddc3f65a30ef75865609f94c433c
amansinha1412 <amansinha1412@mail.com>
2018-08-01 22:35:28 +0530

    second commit of adding view the location feature

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -13,7 +13,9 @@ queue()
   //long  = JSON.parse(long);
   var uluru = {lat: 38.631913, lng: -121.434879}; 
   var map = new google.maps.Map(
-  document.getElementById('map'), {zoom: 14, center: uluru});
+  document.getElementById('map'), {zoom: 13, center: uluru});
+  var geocoder = new google.maps.Geocoder;
+  var infowindow = new google.maps.InfoWindow;
   var zoomLevel =1;
 
   //var l = lat.length;
@@ -50,9 +52,9 @@ var icon2 = {
     var marker = new google.maps.Marker({position: location, map: map , icon:icon,title:'Clock to zoom'});   
      i++;
      marker.addListener('click',function(){
-          map.setZoom(map.getZoom()+2);
-          map.setCenter(marker.getPosition());
-
+          map.setZoom(map.getZoom()+1);
+          map.setCenter(this.getPosition());
+          geocodeLatLng(geocoder,map,infowindow,this.getPosition());
      });
      markers.push(marker);
     // map_bounds.extend(location);
@@ -82,18 +84,27 @@ var icon2 = {
   	var marker_latitude = parseFloat(lat[i]);
     var location = {lat:marker_latitude ,lng: marker_longitude}	
 
-    var marker = new google.maps.Marker({position: location, map: map , icon:image,title:'Clock to zoom'});   
+    var marker = new google.maps.Marker({position: location, map: map , icon:image,title:'Click to zoom'});   
      i++;
      marker.addListener('click',function(){
           map.setZoom(map.getZoom()+1);
-          map.setCenter(marker.getPosition());
-
+          map.setCenter(this.getPosition());
+          geocodeLatLng(geocoder,map,infowindow,this.getPosition());
      });
      markers.push(marker);
     // map_bounds.extend(location);
     }
   });
-     
+     function geocodeLatLng(geocoder,map,infowindow,location){
+         geocoder.geocode({'location':location},function(results,status){
+            if(status==="OK"){
+            	if(results[0]){
+            		var d = document.getElementById("data");
+            		d.innerHTML = results[0].formatted_address;
+            	}
+            }
+        }); 
+     }
   
   
 } 
